88f1dd2788a965c56cf936f4ca14b9727a4dd79a
Sree <sh590@duke.edu>
2022-09-08 17:09:30 -0400

    Wrote driver fn for HDL and output results

--- blood_calculator.py
+++ blood_calculator.py
@@ -14,10 +14,19 @@ def input_HDL():
         
 def check_HDL(HDL_value):
     if HDL_value >= 60:
-        return "Normal":
+        return "Normal"
     elif HDL_value < 40:
         return "Low"
     else:
         return "Borderline Low"
+
+def HDL_driver():
+    hdl_value = input_HDL()
+    answer = check_HDL(hdl_value)
+    output_HDL_result(hdl_value, answer)
+    
+def output_HDL_result(hdl_value,charac):
+    print("The results for an HDL value of {} is this {}".format(hdl_value, charac)
+    
 
 interface()
