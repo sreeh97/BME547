a9142459133f4df3a617c79f88f834be28bd850c
Sree <sh590@duke.edu>
2022-09-08 18:06:39 -0400

    Added LDL Analyzer

--- blood_calculator.py
+++ blood_calculator.py
@@ -2,6 +2,7 @@ def interface():
     print("Blood Calculator")
     print("Options:")
     print("1 - Analyze HDL")
+    print("2 - Analyze LDL")
     print("9 - Quit:")
     keep_running = True
     while keep_running:
@@ -10,6 +11,8 @@ def interface():
             return
         elif choice == "1":
             HDL_driver()
+        elif choice == "2":
+            LDL_driver()
 
 def input_HDL():
     HDL_input = input("Enter the HDL value:")
@@ -23,13 +26,37 @@ def check_HDL(HDL_value):
     else:
         return "Borderline Low"
 
+def output_HDL_result(hdl_value,charac):
+    print("The results for a HDL value of {} is: {}".format(hdl_value, charac))
+
 def HDL_driver():
     hdl_value = input_HDL()
     answer = check_HDL(hdl_value)
     output_HDL_result(hdl_value, answer)
+        
+## LDL
+
+def input_LDL ():
+    LDL_input = input("Enter the LDL value:")
+    return int(LDL_input)
     
-def output_HDL_result(hdl_value,charac):
-    print("The results for an HDL value of {} is: {}".format(hdl_value, charac))
+def check_LDL(LDL_value):
+    if LDL_value < 130:
+        return "Normal"
+    elif 130<= LDL_value < 159:
+        return "Borderline High"
+    elif 160<= LDL_value < 189:
+        return "High"
+    elif LDL_value >= 190:
+        return "Very High"
+        
+def output_LDL_result(ldl_value, charac):
+    print("The result for a LDL value of {} is: {}".format(ldl_value,charac))
+        
+def LDL_driver():
+    ldl_value = input_LDL()
+    answer = check_LDL(ldl_value)
+    output_LDL_result(ldl_value,answer)
     
 
 interface()
