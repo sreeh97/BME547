bace5edd3e3b9223ac66b7448b79690ffe2d4c41
Sree <sh590@duke.edu>
2022-09-08 17:31:58 -0400

    Add HDL option to main interface

--- blood_calculator.py
+++ blood_calculator.py
@@ -1,15 +1,18 @@
 def interface():
     print("Blood Calculator")
     print("Options:")
+    print("1 - Analyze HDL")
     print("9 - Quit:")
     keep_running = True
     while keep_running:
         choice = input("Enter choice: ")
         if choice == "9":
-            return 
+            return
+        elif choice == "1":
+            HDL_driver()
 
 def input_HDL():
-    HDL_input = input("Enter the HDL value:)
+    HDL_input = input("Enter the HDL value:")
     return int(HDL_input)
         
 def check_HDL(HDL_value):
@@ -26,7 +29,7 @@ def HDL_driver():
     output_HDL_result(hdl_value, answer)
     
 def output_HDL_result(hdl_value,charac):
-    print("The results for an HDL value of {} is this {}".format(hdl_value, charac)
+    print("The results for an HDL value of {} is: {}".format(hdl_value, charac))
     
 
 interface()
