992586137cede37fe60e9aeabc8dc040b111ce0f
Sree <sh590@duke.edu>
2022-09-23 13:55:03 -0400

    blood calculator updated with pycodestyle

--- blood_calculator/blood_calculator.py
+++ blood_calculator/blood_calculator.py
@@ -17,10 +17,12 @@ def interface():
         elif choice == "3":
             total_driver()
 
+
 def input_HDL():
     HDL_input = input("Enter the HDL value:")
     return int(HDL_input)
-        
+
+
 def check_HDL(HDL_value):
     if HDL_value >= 60:
         return "Normal"
@@ -29,58 +31,68 @@ def check_HDL(HDL_value):
     else:
         return "Borderline Low"
 
-def output_HDL_result(hdl_value,charac):
+
+def output_HDL_result(hdl_value, charac):
     print("The results for a HDL value of {} is: {}".format(hdl_value, charac))
+
 
 def HDL_driver():
     hdl_value = input_HDL()
     answer = check_HDL(hdl_value)
     output_HDL_result(hdl_value, answer)
-        
-## LDL
 
-def input_LDL ():
+# LDL
+
+
+def input_LDL():
     LDL_input = input("Enter the LDL value:")
     return int(LDL_input)
-    
+
+
 def check_LDL(LDL_value):
     if LDL_value < 130:
         return "Normal"
-    elif 130<= LDL_value < 159:
+    elif 130 <= LDL_value < 159:
         return "Borderline High"
-    elif 160<= LDL_value < 189:
+    elif 160 <= LDL_value < 189:
         return "High"
     elif LDL_value >= 190:
         return "Very High"
-        
+
+
 def output_LDL_result(ldl_value, charac):
-    print("The result for a LDL value of {} is: {}".format(ldl_value,charac))
-        
+    print("The result for a LDL value of {} is: {}".format(ldl_value, charac))
+
+
 def LDL_driver():
     ldl_value = input_LDL()
     answer = check_LDL(ldl_value)
-    output_LDL_result(ldl_value,answer)
-    
+    output_LDL_result(ldl_value, answer)
+
+
 def Total_Cholesterol():
     total = int(input_HDL()) + int(input_LDL())
     return int(total)
-    
+
+
 def Total_check(total):
     if total < 200:
         return "Normal"
-    elif 200<= total < 239:
+    elif 200 <= total < 239:
         return "Borderline high"
     elif total >= 240:
         return "High"
-        
-   
+
+
 def output_Total(total, charac):
     print("The total cholesterol is {} is: {}".format(total, charac))
-    
+
+
 def total_driver():
     value = Total_Cholesterol()
     answer = Total_check(value)
-    output_Total(value,answer)
-    
+    output_Total(value, answer)
+
+
 if __name__ == "__main__":
     interface()
