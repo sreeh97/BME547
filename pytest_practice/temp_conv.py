03a280a6f19d4e42e3bfb7e00f7a8fdd3a5cd3c4
sreeh97 <112418254+sreeh97@users.noreply.github.com>
2022-09-24 21:22:46 -0400

    Delete pytest_practice directory

--- pytest_practice/temp_conv.py
+++
@@ -1,6 +0,0 @@
-# temp_conv.py
-
-def celsius_from_fahrenheit(temp_degC):
-	temp_f = temp_degC * 1.8 + 32
-	return temp_f
-
--- pytest_practice/test_temp_conv.py
+++
@@ -1,5 +0,0 @@
-def test_celcius_from_fahrenheit():
-	from temp_conv import celsius_from_fahrenheit
-	result = celsius_from_fahrenheit(20)
-	expected = 68
-	assert result == expected
